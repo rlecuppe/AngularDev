94a433ab65f347bc05a97f041243c1feb1d1d4a4
rlecuppe <82895368+rlecuppe@users.noreply.github.com>
2022-10-12 16:05:21 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-world app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Hello World 2 !');
   });
 });
